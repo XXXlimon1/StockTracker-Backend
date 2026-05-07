bccee1301e87f07d2e6a1eeacf1b2b50ee6ff134
meryemcolkesen <meryemcolkesen0@gmail.com>
2026-05-08 00:44:09 +0300

    fixed fcm for final time?

--- Services/FcmService.cs
+++ Services/FcmService.cs
@@ -26,8 +26,9 @@ namespace StockTracker.API.Services
 
             if (!string.IsNullOrEmpty(serviceAccountJson))
             {
-                // Render'da \n literal olarak gelebilir, gerçek newline'a çevir
-                var fixedJson = serviceAccountJson.Replace("\\n", "\n");
+                // private_key içindeki literal \n'leri gerçek newline'a çevir
+                var fixedJson = serviceAccountJson.Replace("\n", "
+");
                 credential = GoogleCredential.FromJson(fixedJson)
                     .CreateScoped("https://www.googleapis.com/auth/firebase.messaging");
             }
