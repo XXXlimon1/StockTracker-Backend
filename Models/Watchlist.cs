f642b36654a1b3dc8e51b5a2b74ce7e930c077c3
meryemcolkesen <meryemcolkesen0@gmail.com>
2026-05-15 21:44:06 +0300

    Add Watchlist model

---
+++ Models/Watchlist.cs
@@ -0,0 +1,11 @@
+﻿namespace StockTracker.API.Models
+{
+    public class Watchlist
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Ticker { get; set; } = string.Empty;
+        public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+        public User? User { get; set; }
+    }
+}
